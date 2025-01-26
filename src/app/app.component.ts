e5dc823d616bb34985d4a2745080c5177fd44173
MalicknND <ndiayemalick062@gmail.com>
2025-01-27 00:47:52 +0100

    Ajout du composant FaceSnap avec son template et son style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FaceSnapComponent } from './face-snap/face-snap.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [FaceSnapComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })

512abd5163ca8479b359ea9b6fbd4c20648a2fd8
MalicknND <ndiayemalick062@gmail.com>
2025-01-27 11:59:13 +0100

    Affichez des listes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,35 +10,37 @@ import { FaceSnap } from './models/face-snap';
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
+  faceSnaps!: FaceSnap[];
+
   mySnap!: FaceSnap;
   myOtherSnap!: FaceSnap;
   myLastSnap!: FaceSnap;
 
   ngOnInit(): void {
-    this.mySnap = new FaceSnap(
-      'Face Snap',
-      'This is a simple face snap application',
-      'https://cdn.pixabay.com/photo/2024/12/03/08/08/ai-generated-9241538_1280.jpg',
-      new Date(),
-      5
-    );
+    this.faceSnaps = [
+      new FaceSnap(
+        'Face Snap',
+        'This is a simple face snap application',
+        'https://cdn.pixabay.com/photo/2024/12/03/08/08/ai-generated-9241538_1280.jpg',
+        new Date(),
+        5
+      ),
+      new FaceSnap(
+        'My Other Snap',
+        'This is a simple face snap application',
+        'https://cdn.pixabay.com/photo/2019/09/11/16/09/artificial-intelligence-4469138_1280.jpg',
+        new Date(),
+        10
+      ),
+      new FaceSnap(
+        'My Last Snap',
+        'This is a simple face snap application',
+        'https://cdn.pixabay.com/photo/2018/03/15/10/35/website-3227784_1280.jpg',
+        new Date(),
+        15
+      ),
+    ];
 
-    this.myOtherSnap = new FaceSnap(
-      'My Other Snap',
-      'This is a simple face snap application',
-      'https://cdn.pixabay.com/photo/2019/09/11/16/09/artificial-intelligence-4469138_1280.jpg',
-      new Date(),
-      10
-    );
-
-    this.myLastSnap = new FaceSnap(
-      'My Last Snap',
-      'This is a simple face snap application',
-      'https://cdn.pixabay.com/photo/2018/03/15/10/35/website-3227784_1280.jpg',
-      new Date(),
-      15
-    );
-
-    this.myOtherSnap.setLocation('A Dakar');
+    this.faceSnaps[1].setLocation('A Dakar');
   }
 }

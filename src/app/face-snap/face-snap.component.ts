42a41e8f7cd967ad4e4849f24bea48c2ebc8864a
MalicknND <ndiayemalick062@gmail.com>
2025-01-29 23:48:15 +0100

    Injectez votre service 1

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,17 +1,11 @@
 import { Component, Input, type OnInit } from '@angular/core'; // Importation des décorateurs, types et interfaces nécessaires pour créer un composant Angular.
 import type { FaceSnap } from '../models/face-snap'; // Importation du modèle FaceSnap pour typer les données.
-import {
-  CurrencyPipe,
-  DatePipe,
-  NgClass,
-  NgStyle,
-  UpperCasePipe,
-} from '@angular/common';
+import { DatePipe, NgClass, NgStyle, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-face-snap', // Définit le sélecteur HTML utilisé pour insérer ce composant dans une page.
   standalone: true, // Permet de définir ce composant comme un composant autonome (pas besoin d'être déclaré dans un module Angular).
-  imports: [NgStyle, NgClass, UpperCasePipe, DatePipe, CurrencyPipe], // Liste des imports nécessaires à ce composant (actuellement vide).
+  imports: [NgStyle, NgClass, UpperCasePipe, DatePipe], // Liste des imports nécessaires à ce composant (actuellement vide).
   templateUrl: './face-snap.component.html', // Chemin vers le fichier de template HTML associé.
   styleUrl: './face-snap.component.scss', // Chemin vers le fichier de styles SCSS associé.
 })
